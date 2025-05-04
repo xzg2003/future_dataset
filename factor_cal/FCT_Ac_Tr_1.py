d48d67c8005ffd87b1751a1a90637175c8fb73bb
IVANLEE <lee162286@gmail.com>
2025-05-04 13:42:45 +0800

    FCT_Ac_Tr_1 计算器
    增加了 mindiff 的处理部分；对算法进行了优化，提高计算效率；修改存储的内容，改为存储 datetime

--- factor_cal/FCT_Ac_Tr_1.py
+++ factor_cal/FCT_Ac_Tr_1.py
@@ -8,17 +8,34 @@ class FCT_Ac_Tr_1:
         self.factor_name = 'FCT_Ac_Tr_1'
 
     def formula(self, param):
-        # 从字典中提取DataFrame
+        # 从字典中提取 DataFrame
         df = param.get('df', None)
         if df is None:
-            raise ValueError("参数 'param' 中缺少 'df' 键或其值为空")
+            raise ValueError("no 'df' in param")
 
-        # 确保df是pandas.DataFrame类型
+        # 确保 df 是 pandas.DataFrame 类型
         if not isinstance(df, pandas.DataFrame):
-            raise TypeError("'df' 必须是 pandas.DataFrame 类型")
+            raise TypeError("df must be DataFrame")
 
-        # 从字典中读取参数
+        """
+        从字典中读取 length
+        获取 instrument 名称，用于提取mindiff中的数据
+        """
         length = param.get('length', None)
+        if length is None:
+            raise ValueError("param missing 'length'")
+        print(f"Using length: {length}")
+
+        # 获取instrument
+        instrument = param.get('instrument', None)
+        if instrument is None:
+            raise ValueError("param miss instrument")
+
+        # 获取mindiff
+        mindiff = param.get('mindiff', None)
+        if mindiff is None:
+            raise ValueError(f"param miss mindiff for instrument: {param.get('instrument', 'unknown')}")
+        print(f"Using mindiff: {mindiff}")
 
         # 计算价格均值，这里取价格的最高价与最低价的平均
         df['MP'] = (df['high'] + df['low']) / 2
@@ -39,14 +56,20 @@ class FCT_Ac_Tr_1:
             axis=1
         )
 
+        """
         # 计算输出结果，这里没有最小变动单位，没有办法进行分类处理
         df[f'FCT_Ac_Tr_1@{length}'] = df['AC'] / df['Tr'].rolling(window=length).mean()
+        """
 
-        f'''
-        以下是正确的处理函数
-        df[f'FCT_Ac_Tr_1@{length}'] = numpy.where(df['Tr'].rolling(window=length).mean() < df['mindiff'], 0, df['AC'] / df['Tr'].rolling(length).mean())
-        '''
+        rolling_mean_tr = df['Tr'].rolling(window=length).mean().fillna(0)
+
+        # 以下是正确的处理函数
+        df[f'FCT_Ac_Tr_1@{length}'] = numpy.where(
+            rolling_mean_tr < mindiff,
+            0,
+            df['AC'] / rolling_mean_tr
+        )
 
         # 返回结果
-        result = df[['trading_date', f'FCT_Ac_Tr_1@{length}']].copy()
+        result = df[['datetime', f'FCT_Ac_Tr_1@{length}']].copy()
         return result
