6c0666c4710ed4f5f4dd6abd5378cd41ca6c39e1
IVANLEE <lee162286@gmail.com>
2025-05-13 14:49:55 +0800

    OvernightMOM 因子计算器
    OvernightMOM 因子计算器，隔夜动量因子

---
+++ factor_cal/OvernightMOM.py
@@ -0,0 +1,31 @@
+# OvernightMOM：隔夜动量因子，衡量今开盘价相对昨收盘价的对数收益率
+
+import pandas
+import numpy
+import os
+
+# 设置工作目录为当前脚本所在的目录
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+class OvernightMOM:
+    def __init__(self):
+        self.factor_name = 'OvernightMOM'
+
+    def formula(self, param):
+        # 从字典中提取 DataFrame
+        df = param.get('df', None)
+        if df is None:
+            raise ValueError("no 'df' in param")
+        if not isinstance(df, pandas.DataFrame):
+            raise TypeError("df must be DataFrame")
+
+        # 计算做完收盘价
+        df['close_pre'] = df['close'].shift(1)
+
+        # 计算隔夜动量（今开盘/昨收盘的对数收益率）
+        df['OvernightMOM'] = numpy.log(df['open'] / df['close_pre'])
+        df['OvernightMOM'] = df['OvernightMOM'].replace([numpy.inf, -numpy.inf], numpy.nan).fillna(0)
+
+        # 返回结果
+        result = df[['datetime', 'OvernightMOM']].copy()
+        return result
