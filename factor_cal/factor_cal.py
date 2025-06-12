eeb79df5dc8d6a7a3fd06e1926a5df3eca49d5a9
IVANLEE <lee162286@gmail.com>
2025-06-12 23:12:41 +0800

    主程序入口调整

--- factor_cal/factor_cal.py
+++ factor_cal/factor_cal.py
@@ -163,8 +163,29 @@ if __name__ == "__main__":
     k_line_type = '5m'
     lengths = [10, 20, 40, 80, 120, 180]
 
+    # 最小变化单位文件路径
+    mindiff_file_path = './data/mindiff/mindiff.csv'
+
+    # 初始化 mindiff 字典
+    instruments_mindiff = {}
+
+    # 读取 CSV 文件
+    with open(mindiff_file_path, mode='r', encoding='utf-8') as mindiff_file:
+        reader = csv.DictReader(mindiff_file)
+        for row in reader:
+            instrument = row['instrument'].strip()
+            mindiff = row['mindiff']
+            try:
+                # 确保 instrument 和 mindiff 不为空
+                if instrument and mindiff:
+                    instruments_mindiff[instrument] = float(mindiff)
+            except ValueError:
+                print(f"Skip invalid data:{row}")
+
+    print(f"Loaded instruments_mindiff:{instruments_mindiff}")
+
     # 创建因子计算器实例，以调用内部的函数
-    calculator = factor_calculator(instruments, k_line_type, lengths, mindiff)
+    calculator = factor_calculator(instruments, k_line_type, lengths, instruments_mindiff)
 
     # 计算所有因子
     calculator.factors_cal()
